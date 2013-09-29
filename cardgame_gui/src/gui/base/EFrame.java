b516d51da9360d60ade34d9ede937e51c3103179
Emil Södergren <emilsodergren@telia.com>
2013-09-29 13:32:07 +0200

    First commit of GUI project

---
+++ cardgame_gui/src/gui/base/EFrame.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package gui.base;
+
+import javax.swing.JFrame;
+
+/**
+ * @author MrE
+ *
+ */
+public class EFrame extends JFrame {
+
+	private static final long serialVersionUID = 5143544822454599810L;
+	
+	public EFrame() {
+		super();
+	}
+	
+	public EFrame(String s) {
+		super(s);
+	}
+
+}
