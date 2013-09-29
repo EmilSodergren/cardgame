c4f24f17e0e23db862cbe971830304366a0bdc64
Emil Södergren <emilsodergren@telia.com>
2013-09-29 18:14:17 +0200

    Added a LoggerWindow

--- cardgame_logging/src/cardgame/logging/gui/LoggerWindow.java
+++ cardgame_logging/src/cardgame/logging/gui/LoggerWindow.java
@@ -3,8 +3,32 @@
  */
 package cardgame.logging.gui;
 
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 import framework.graphics.guicomponents.EFrame;
 
 public class LoggerWindow extends EFrame{
 
+	private static final long serialVersionUID = 2464154933560375812L;
+	
+	public LoggerWindow() {
+		super("Log");
+		setName("LoggerWindow");
+		
+		
+		setSize(640, 320);
+		initComponents();
+		
+		this.setVisible(true);
+	}
+
+	private void initComponents() {
+		JScrollPane scrollpane = new JScrollPane();
+		JTextArea textArea = new JTextArea();
+
+		scrollpane.add(textArea);
+		
+		add(scrollpane);
+	}
 }
