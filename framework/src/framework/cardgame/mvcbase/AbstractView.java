6deacc1546ceb232952ae002d8e274c32a2242c6
Emil Södergren <emilsodergren@telia.com>
2013-10-13 10:31:41 +0200

    Added a testframework to build my program structure on

---
+++ framework/src/framework/cardgame/mvcbase/AbstractView.java
@@ -0,0 +1,13 @@
+/**
+ * Created by MrE on 10 okt 2013
+ */
+package framework.cardgame.mvcbase;
+
+import java.beans.PropertyChangeEvent;
+
+public class AbstractView {
+
+	public void update(PropertyChangeEvent evt) {
+		
+	}
+}
