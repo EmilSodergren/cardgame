0feb2d4acbbe359263f1d950b45e3a8e0063046e
Emil Södergren <emilsodergren@telia.com>
2013-10-26 16:59:51 +0200

    Renamed the images in resources. Added logging

--- cardgame_cards/src/cardgame/cards/gui/CardGuiBase.java
+++ cardgame_cards/src/cardgame/cards/gui/CardGuiBase.java
@@ -3,25 +3,36 @@
  */
 package cardgame.cards.gui;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 import framework.graphics.guicomponents.EComponent;
+import framework.logging.logger.CardGameLogger;
 
 public class CardGuiBase extends EComponent {
 
 	private static final long serialVersionUID = 5322618823703871299L;
 	
+	private CardGameLogger logger = new CardGameLogger(CardGuiBase.class);
+	
 	protected Image bgImage;
 	
 	public CardGuiBase(String imageName) {
 		super();
+		String filePath = String.format("cardgame/cards/resources/%s", imageName);
 		try {
-			bgImage = ImageIO.read(CardGuiBase.class.getClassLoader().getResource("cardgame/cards/resources/" + imageName));
+			bgImage = ImageIO.read(CardGuiBase.class.getClassLoader().getResource(filePath));
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.err(String.format("The file %s can not be found!", filePath));
 		}
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawImage(bgImage, 100, 100, null);
 	}
 }
